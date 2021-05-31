46e22146dea1a0092350305b719099bb14bc0712
danielchan <danielchan@192.168.0.166>
2021-05-31 10:34:46 -0400

    Mise à jour

--- daniel_juin/src/daniel_juin/qwe.java
+++ daniel_juin/src/daniel_juin/qwe.java
@@ -2,12 +2,14 @@ package daniel_juin;
 
 
 
-import java.awt.Label;
+
 
 import javafx.application.Application;
 import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+
 import javafx.stage.Stage;
 
 public class qwe extends Application {
@@ -17,17 +19,26 @@ public class qwe extends Application {
 	{
 		Group group = new Group();
 		Scene scene = new Scene(group, 400, 400);
+		
+		Label label1 = new Label ();
+		
+		label1.setTranslateX(150);
+		label1.setTranslateY(200);
+		
+		Label label2 = new Label ();
+		
+		label2.setTranslateX(300);
+		label2.setTranslateY(200);
+		
 		TextField txt1 = new TextField ();
 		txt1.setPrefWidth(100);
 		txt1.setPrefHeight(20);
 		txt1.setLayoutX(20);
 		txt1.setLayoutY(50);
 		
-		Label lbl1= new Label();
-		lbl.With(20);
-		lbl1.setPrefHeight(20);
-		txt1.setLayoutX(20);
-		txt1.setLayoutY(50);
+		
+		
+		
 		
 
 		
@@ -39,38 +50,30 @@ public class qwe extends Application {
 		
 	
 		
-		TextField txt3 = new TextField ();
-		
-		txt3.setPrefWidth(100);
-		txt3.setPrefHeight(20);
-		txt3.setLayoutX(270);
-		txt3.setLayoutY(50);
 		
 		txt1.textProperty().addListener((observable, oldValue, newValue)->
 		{
-			int c= Integer.parseInt(newValue);
+			label1.textProperty().bind(txt1.textProperty());
 		});
 		
 		txt2.textProperty().addListener((observable, oldValue, newValue)->
 		{
-			int d= Integer.parseInt(newValue);
+			label2.textProperty().bind(txt2.textProperty());
 		});
 		
 		
-		txt3.textProperty().addListener((observable, oldValue, newValue)->
-		{
-			
-		});
+		
 		
 		
 		group.getChildren().add(txt1);
 		group.getChildren().add(txt2);
-		group.getChildren().add(txt3);
-		
+		group.getChildren().add(label1);
+	
 		
 		
 		primaryStage.setScene(scene);
 		primaryStage.show();
+	
 	}
 
 	public static void main(String[] args) {
