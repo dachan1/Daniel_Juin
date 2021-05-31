d1ebdd5348d799e49b2afe74fb7be7e15f7873e4
danielchan <danielchan@192.168.0.166>
2021-05-31 18:12:22 -0400

    Mise à jour

--- daniel_juin/src/daniel_juin/qwe.java
+++ daniel_juin/src/daniel_juin/qwe.java
@@ -5,6 +5,9 @@ package daniel_juin;
 
 
 import javafx.application.Application;
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.value.ObservableValue;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
@@ -17,17 +20,21 @@ public class qwe extends Application {
 	@Override
 	public void start(Stage primaryStage) 
 	{
+		
+		//int x=0, res=0;
+		
+		
 		Group group = new Group();
 		Scene scene = new Scene(group, 400, 400);
 		
 		Label label1 = new Label ();
 		
-		label1.setTranslateX(150);
+		label1.setTranslateX(50);
 		label1.setTranslateY(200);
 		
 		Label label2 = new Label ();
 		
-		label2.setTranslateX(300);
+		label2.setTranslateX(125);
 		label2.setTranslateY(200);
 		
 		TextField txt1 = new TextField ();
@@ -35,6 +42,21 @@ public class qwe extends Application {
 		txt1.setPrefHeight(20);
 		txt1.setLayoutX(20);
 		txt1.setLayoutY(50);
+		
+		Label label3 = new Label ("+");
+		
+		label3.setTranslateX(100);
+		label3.setTranslateY(200);
+		
+		Label label4 = new Label ("=");
+		
+		label4.setTranslateX(200);
+		label4.setTranslateY(200);
+		
+		Label label5 = new Label ("=");
+		
+		label5.setTranslateX(250);
+		label5.setTranslateY(200);
 		
 		
 		
@@ -53,24 +75,34 @@ public class qwe extends Application {
 		
 		txt1.textProperty().addListener((observable, oldValue, newValue)->
 		{
-			label1.textProperty().bind(txt1.textProperty());
+		 	label1.textProperty().bind(txt1.textProperty());
+		 	
+				
 		});
+		
+		
 		
 		txt2.textProperty().addListener((observable, oldValue, newValue)->
 		{
 			label2.textProperty().bind(txt2.textProperty());
+			
+			 //res= (Integer.parseInt(txt1.getText()));
+			
+			//String temp = label1.getText(); 
+			//int temp1 = Integer.parseInt(temp);
+			//System.out.println(temp1);
+			//label4.setText(String.format("%.4f",res+x));
+			
 		});
 		
 		
-		
-		
-		
+    	
 		group.getChildren().add(txt1);
 		group.getChildren().add(txt2);
 		group.getChildren().add(label1);
-	
-		
-		
+		group.getChildren().add(label2);
+		group.getChildren().add(label3);
+		group.getChildren().add(label4);
 		primaryStage.setScene(scene);
 		primaryStage.show();
 	
